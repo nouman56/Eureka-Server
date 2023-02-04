a6eb464f13162a15ecfa3ca19ae1e0169fdf321d
nouman56 <noumanafzaal56@gmail.com>
2023-02-04 18:01:37 +0500

    first commit

---
+++ src/main/java/com/ascertia/eurikeserver/EurikeServerApplication.java
@@ -0,0 +1,24 @@
+package com.ascertia.eurikeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurikeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurikeServerApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
